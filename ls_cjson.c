56509f75fc57fd38ae52ccdde745dc028b6a8617
wz <1208827324@qq.com>
2022-09-12 23:59:10 +0800

    change

--- ls_cjson.c
+++ ls_cjson.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-cjon* cjson_parse(const char* json_string)
+cjson* cjson_parse(const char* json_string)
 {
     if(json_string == NULL) { return NULL; }
 
     return cJSON_Parse(json_string);
 }
 
-void cjson_delete(cjon* root)
+void cjson_delete(cjson* root)
 {
     if(root != NULL)
     {
@@ -17,14 +17,14 @@ void cjson_delete(cjon* root)
     }
 }
 
-char* cjson_prirnt(const cjon* root)
+char* cjson_prirnt(const cjson* root)
 {
     if(root == NULL) { return ""; }
 
     return cJSON_Print(root);
 }
 
-char* cjson_prirnt_unformatted(const cjon* root)
+char* cjson_prirnt_unformatted(const cjson* root)
 {
     if(root == NULL) { return ""; }
 
@@ -39,7 +39,7 @@ void cjson_free(const char* root_string)
     }
 }
 
-bool cjson_to_bool(const cjon* obj)
+bool cjson_to_bool(const cjson* obj)
 {
     bool result = false;
 
@@ -59,7 +59,7 @@ bool cjson_to_bool(const cjon* obj)
     return result;
 }
 
-int32_t cjson_to_i32(const cjon* obj)
+int32_t cjson_to_i32(const cjson* obj)
 {
     int32_t result = 0;
 
@@ -75,7 +75,7 @@ int32_t cjson_to_i32(const cjon* obj)
     return result;
 }
 
-uint32_t cjson_to_u32(const cjon* obj)
+uint32_t cjson_to_u32(const cjson* obj)
 {
     uint32_t result = 0;
 
@@ -91,7 +91,7 @@ uint32_t cjson_to_u32(const cjon* obj)
     return result;
 }
 
-double cjson_to_double(const cjon* obj)
+double cjson_to_double(const cjson* obj)
 {
     double result = 0;
 
@@ -108,7 +108,7 @@ double cjson_to_double(const cjon* obj)
 }
 
 // if json root is delete, the string can't be used
-char* cjson_to_string(const cjon* obj)
+char* cjson_to_string(const cjson* obj)
 {
     char* result = "";
 
@@ -121,7 +121,7 @@ char* cjson_to_string(const cjon* obj)
 }
 
 // copy json string to buffer with max size
-void cjson_to_string_copy(const cjon* obj, char* buffer, int32_t size)
+void cjson_to_string_copy(const cjson* obj, char* buffer, int32_t size)
 {
     int32_t length = 0;
 
@@ -138,5 +138,29 @@ void cjson_to_string_copy(const cjon* obj, char* buffer, int32_t size)
     }
 }
 
+int32_t cjson_array_size(const cjson* array)
+{
+    int32_t result = 0;
 
+    if(cJSON_IsArray(array))
+    {
+        result = cJSON_GetArraySize(array);
+    }
+
+    return result;
+}
+
+// array index begin with 0
+cjson* cjson_to_array(const cjson* array, int32_t index)
+{
+    cjson* result = NULL;
+    int32_t array_size = cjson_array_size(array);
+
+    if( array_size && index <= (array_size - 1) )
+    {
+        result = cJSON_GetArrayItem(array, index);
+    }
+
+    return result;
+}
 
--- ls_cjson.h
+++ ls_cjson.h
@@ -9,21 +9,25 @@
 extern "C" {
 #endif
 
-typedef cJSON cjon;
+typedef cJSON cjson;
 
-cjon* cjson_parse(const char* json_string);
-void  cjson_delete(cjon* root);
+cjson* cjson_parse(const char* json_string);
+void  cjson_delete(cjson* root);
 
-char* cjson_prirnt(const cjon* root);
-char* cjson_prirnt_unformatted(const cjon* root);
+char* cjson_prirnt(const cjson* root);
+char* cjson_prirnt_unformatted(const cjson* root);
 void  cjson_free(const char* root_string);
 
-bool cjson_to_bool(const cjon* obj);
-int32_t cjson_to_i32(const cjon* obj);
-uint32_t cjson_to_u32(const cjon* obj);
-double cjson_to_double(const cjon* obj);
-char* cjson_to_string(const cjon* obj);
-void cjson_to_string_copy(const cjon* obj, char* buffer, int32_t size);
+bool cjson_to_bool(const cjson* obj);
+int32_t cjson_to_i32(const cjson* obj);
+uint32_t cjson_to_u32(const cjson* obj);
+double cjson_to_double(const cjson* obj);
+char* cjson_to_string(const cjson* obj);
+void cjson_to_string_copy(const cjson* obj, char* buffer, int32_t size);
+
+int32_t cjson_array_size(const cjson* array);
+cjson* cjson_to_array(const cjson* array, int32_t index);
+
 
 #ifdef __cplusplus
 }
